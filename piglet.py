c5aaea572b35023c191a243b8a48f057ab189e88
quilder <quilder@users.noreply.github.com>
2017-03-02 15:42:42 +0100

    Add possibility to read from file. Fixed #3

--- piglet.py
+++ piglet.py
@@ -6,7 +6,18 @@ vowels = ["a","o","u","e","i","y", "å", "ä", "ö"]
 def pig(word):
     return "igwordpay"
 
+def read_file(filename):
+    with open(filename) as f:
+        # Remove newlines
+        lines = [line.rstrip('\n') for line in f.readlines()]
+        # Remove empty strings
+        lines = filter(None, lines)
+        return lines
+
+def main():
+    lines = read_file("example_wordlist")
+    for line in lines:
+        print(pig(line))
+
 if __name__ == "__main__":
-    word = str(raw_input("word: "))
-    pigword = pig(word)
-    print pigword
+    main()
