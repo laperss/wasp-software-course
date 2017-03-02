16219de153bb6171d2aa677b52acff3732f3ca1d
Linnea Persson <laperss@kth.se>
2017-03-02 15:38:10 +0100

    Add test cases for pig function

--- piglet_test.py
+++ piglet_test.py
@@ -1,9 +1,11 @@
 import unittest
-from piglet' import pig
+from piglet import pig
 
 class TestMain(unittest.TestCase):
     def test_pig(self):
-        self.assertEqual(pig("random"), "igwordpay")
+        self.assertEqual(pig("linnea"), "innealay")
+        self.assertEqual(pig("hallo"), "allohay")
+        self.assertEqual(pig("engineering"), "engineeringway")
 
 
 if __name__ == "__main__" :
